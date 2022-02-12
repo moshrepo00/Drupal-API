05bfac0ef75f53509e71f91e82550cc6c41a0d11
mo <moshpit2241@gmail.com>
2022-02-12 17:48:13 +0800

    testing google functionality

--- web/modules/custom/api_user/src/Form/QueueTestForm.php
+++ web/modules/custom/api_user/src/Form/QueueTestForm.php
@@ -21,10 +21,25 @@ class QueueTestForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+      \Drupal::messenger()->addMessage('FORM BUILT!!!!');
+    $form['#attached']['library'][] = 'api_user/google-functionality';
+
+    $form['captcha_button'] = array(
+      '#type' => 'button',
+      '#value' => t('Alternative Action'),
+      '#attributes' => [
+        'class' => ['g-recaptcha'],
+        'data-sitekey' => ['reCAPTCH_6LcLR3MeAAAAAIMThlgpmc_m5vVnmWWEQmyl_jbu'],
+        'data-callback' => ['onSubmit'],
+        'data-action' => ['submit'],
+      ],
+    );
     $form['submit'] = [
       '#type' => 'submit',
+      '#prefix' => '<div class="g-recaptcha" data-sitekey="6LccwHMeAAAAALka668fPwunJsV8LtcfxskNmEWu"></div>',
       '#value' => $this->t('Submit'),
     ];
+
 
     return $form;
   }
@@ -44,13 +59,14 @@ class QueueTestForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Display result.
-    $queue = \Drupal::queue('log_queue');
-    for ($i = 0; $i < 10; $i++) {
-      $queue->createItem($i);
-    }
-    foreach ($form_state->getValues() as $key => $value) {
-      \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
-    }
+    // TODO remove comments when testing queue
+//    $queue = \Drupal::queue('log_queue');
+//    for ($i = 0; $i < 10; $i++) {
+//      $queue->createItem($i);
+//    }
+//    foreach ($form_state->getValues() as $key => $value) {
+//      \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
+//    }
   }
 
 }
