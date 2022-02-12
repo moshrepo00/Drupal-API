f8b784850aa6144ef17f160d71f612e85f1d39fc
mo <moshpit2241@gmail.com>
2022-02-12 20:37:03 +0800

    recpatcha

--- web/modules/custom/api_user/js/recaptcha.js
+++ web/modules/custom/api_user/js/recaptcha.js
@@ -13,4 +13,5 @@ console.log(document.getElementById("queue-test-form"));
 function onSubmit() {
   console.log('submit button triggered!!!');
   // document.getElementById("queue-test-form").submit();
+  
 }
